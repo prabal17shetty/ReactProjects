2fbaf3ae820315e5f4f51b6f55698350b2179ee8
prabal17shetty <prabal17shetty@gmail.com>
2021-04-04 01:06:58 +0530

    Final Commit

---
+++ .bash_history
@@ -0,0 +1,9 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install zsh
+zsh
+sudo apt-get install curl
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo apt-get install git
+git version
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
---
+++ Desktop/Interview.java
@@ -0,0 +1,25 @@
+import java.io*;
+
+class Main{
+
+
+class BMW{
+    // void Models(
+    //     System.out.print("List of Models");r
+    // void componets(){
+    //     System.out.print("Tyres, V12 Engine, Lights, Brakes");
+
+    BMW car = new car();
+    car.engine();
+    car.tyres();
+    car.lights();
+    car.breakes();
+    }
+}
+// class bmwYseries extends BMW{
+
+
+//     bmwYseries newCar = new newCar();
+//     newCar.Rooftop();
+
+// }
